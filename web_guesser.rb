c860a89c93d50dcdc1c9ee26ce5d7aef53b0bf33
Amara Lovato <amaracuenca@gmail.com>
2017-01-18 11:31:35 -0700

    separates check_guess method

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,18 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  guess = params["guess"].to_i
-  number = rand(100).to_i
-  if guess - number > 5
-    message = "Way too high!"
-  elsif number - guess > 5
-    message = "Way too low!"
-  elsif guess > number
-    message = "Too high!"
-  elsif guess < number
-    message = "Too low!"
-  else
-    message = "The SECRET NUMBER is #{number}"
-  end
+  guess = params["guess"]
+  message = check_guess(guess)
+  number = @number
   erb :index, :locals => {:number => number, :message => message}
+end
+
+def check_guess(guess)
+  @number = rand(100).to_i
+  if guess.to_i - @number > 5
+    "Way too high!"
+  elsif @number - guess.to_i > 5
+    "Way too low!"
+  elsif guess.to_i > @number
+    "Too high!"
+  elsif guess.to_i < @number
+    "Too low!"
+  else
+    "The SECRET NUMBER is #{@number}"
+  end
 end
